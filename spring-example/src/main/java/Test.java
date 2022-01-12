dc29ff90f30d651cfbb3e1d4471f48a295ea41b9
will_mac <will@will-macdeMacBook-Pro.local>
2022-01-12 23:27:03 +0800

    导入spring源码，写测试方法

---
+++ spring-example/src/main/java/Test.java
@@ -0,0 +1,11 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext();
+//		context.register(Bean.class);
+		context.refresh();
+		System.out.println(context.getBean(Bean.class));
+	}
+}
